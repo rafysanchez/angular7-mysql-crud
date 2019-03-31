6bcf703c42cc3dbd2dd472dc4d1b405d89aa8b14
rafael sanchez port <rafysanchez@hotmail.com>
2019-03-30 22:43:55 -0300

    adjs

--- database/database.sql
+++ database/database.sql
@@ -5,7 +5,7 @@ USE ng_games;
 CREATE TABLE game (
     id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(180),
-    description VARCHAR(255),
+    description VARCHAR (255),
     image VARCHAR(200),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
